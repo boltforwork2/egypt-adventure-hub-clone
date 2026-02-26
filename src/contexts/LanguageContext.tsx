0cbf89fc535c2f07f74609907e5aecb20ea53600
boltforwork37 <boltforwork37@gmail.com>
2026-02-26 15:50:31 +0200

    Updated LanguageContext.tsx

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -12,7 +12,7 @@ const LanguageContext = createContext<LanguageContextType | undefined>(undefined
 export const LanguageProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { i18n } = useTranslation();
   const [language, setLanguage] = useState<string>('en');
-  const availableLanguages = ['en', 'de', 'fr','it'];
+  const availableLanguages = ['en', 'de', 'fr','it','pl'];
 
   useEffect(() => {
     const savedLanguage = localStorage.getItem('language') || 'en';
