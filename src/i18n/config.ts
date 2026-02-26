6573fd04a4fac98776b80744543e8bf64806cd15
boltforwork37 <boltforwork37@gmail.com>
2026-02-26 15:14:26 +0200

    Updated config.ts

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -5,6 +5,7 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import en from './locales/en.json';
 import de from './locales/de.json';
 import fr from './locales/fr.json';
+import it from './locales/it.json';
 
 i18n
   .use(LanguageDetector)
